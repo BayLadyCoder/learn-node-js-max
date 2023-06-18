844e03cd638764cfe2e3fe47a73d9535a9a262cc
BayC <rachada_32@hotmail.com>
2023-06-18 16:49:00 -0400

    Add path to app.use on 1st argument

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const express = require('express');
 const app = express();
 
 // use middleware
-app.use((req, res, next) => {
-  console.log('hello from middleware');
-
+app.use('/add-product', (req, res, next) => {
+  console.log('hello from 2nd middleware');
+  res.send('<h1>Add Product Page!</h1>');
+  // we don't use next because we already send response, don't need to send two response
   // use next to go to the next middleware
-  next();
+  // next();
 });
 
-app.use((req, res, next) => {
-  console.log('hello from 2nd middleware');
+app.use('/', (req, res, next) => {
+  console.log('hello from middleware');
   res.send('<h1>Hello from Express!</h1>');
 });
 
