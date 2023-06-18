fa3780e3d3251753064dd5ed73b386b6d48a0c36
BayC <rachada_32@hotmail.com>
2023-06-18 16:22:23 -0400

    Use app.use for middleware

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const express = require('express');
 
 const app = express();
 
+// use middleware
+app.use((req, res, next) => {
+  console.log('hello from middleware');
+
+  // use next to go to the next middleware
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('hello from 2nd middleware');
+});
+
 const server = http.createServer(app);
 
 const port = 3000;
