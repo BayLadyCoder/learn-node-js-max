e1bf9443c612b76eaaae73fd32a23d5b0d477e46
BayC <rachada_32@hotmail.com>
2023-06-18 17:42:08 -0400

    Filter routes by route prefix path

--- routes/admin.js
+++ routes/admin.js
@@ -5,12 +5,12 @@ const router = express.Router();
 // use `router` instead of `app`, and we can use it as a middleware in app.js
 router.get('/add-product', (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>'
   );
 });
 
-router.post('/product', (req, res, next) => {
-  console.log('/product', { body: req.body });
+router.post('/add-product', (req, res, next) => {
+  console.log('POST /add-product', { body: req.body });
   res.redirect('/');
 });
 
