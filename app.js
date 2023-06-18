cd39c712fcea18171b30e31309527d6b17fc70ca
BayLadyCoder <rachada_32@hotmail.com>
2023-06-18 16:38:56 -0400

    Use res.send to send html text response

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('hello from 2nd middleware');
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 const server = http.createServer(app);
