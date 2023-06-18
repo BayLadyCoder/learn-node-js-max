466abbf6f449db2075aa71b8afe723fdb6cbfc1f
BayC <rachada_32@hotmail.com>
2023-06-18 17:43:18 -0400

    Add comments to admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 // use `router` instead of `app`, and we can use it as a middleware in app.js
+// GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
   res.send(
     '<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>'
   );
 });
 
+// POST /admin/add-product
 router.post('/add-product', (req, res, next) => {
   console.log('POST /add-product', { body: req.body });
   res.redirect('/');
