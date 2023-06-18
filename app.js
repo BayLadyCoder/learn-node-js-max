e15b0bacc3bba24438cd9792dc1abf4ef481629e
BayC <rachada_32@hotmail.com>
2023-06-18 16:43:31 -0400

    Use app.listen(port) to createServer and listen

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-
 const port = 3000;
-server.listen(port);
+
+// app.listen does 2 things, createServer and listen
+app.listen(port);
