a9067e866c70a4640e90f0a4df43a748bd4d041d
BayC <rachada_32@hotmail.com>
2023-06-18 16:52:43 -0400

    Create form in /add-product and redirect to home page on submit form

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const app = express();
 // use middleware
 app.use('/add-product', (req, res, next) => {
   console.log('hello from 2nd middleware');
-  res.send('<h1>Add Product Page!</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>'
+  );
   // we don't use next because we already send response, don't need to send two response
   // use next to go to the next middleware
   // next();
+});
+
+app.use('/product', (req, res, next) => {
+  console.log('/product', { body: req.body });
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
