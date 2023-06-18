073a566d0dd69e01f4e858e2d5f4bf783aa3867b
BayC <rachada_32@hotmail.com>
2023-06-18 02:27:15 -0400

    Decouple requestHandler to its own file

---
+++ routes.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+  const { url, method, headers } = req;
+
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Submit</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    console.log({ url, method });
+
+    const body = [];
+
+    // need to have `name` attribute on <input> to trigger req.on('data') callback
+    req.on('data', (chunk) => {
+      console.log({ chunk });
+      body.push(chunk);
+    });
+
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log({ message });
+
+      // writeFile asynchronously
+      fs.writeFile('message.txt', message, (err) => {
+        // redirect to home page after POST message is done
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hellp from my Node.js!</h1></body>');
+  res.write('</html>');
+  res.end();
+
+  // to quit the server
+  // process.exit;
+};
+
+module.exports = { requestHandler };
