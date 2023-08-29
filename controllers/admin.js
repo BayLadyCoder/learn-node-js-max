33afd9dab668ef51934743c88927529183eab25a
BayC <rachada_32@hotmail.com>
2023-08-28 20:05:28 -0400

    Show product data in edit product form

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,21 +4,26 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
   });
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
+  const { productId } = req.params;
   // console.log({ query: req.query, params: req.params });
 
-  if (!editMode) {
+  if (!editMode || !productId) {
     return res.redirect('/');
   }
 
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode,
+  Product.findById(productId, (product) => {
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product,
+    });
   });
 };
 
