cda46f2e9e5ba1f279f56acb233ccef71df4f242
BayC <rachada_32@hotmail.com>
2023-09-04 00:38:56 -0400

    Use require('sequelize').Sequelize to fix intellisense

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize').Sequelize;
 
 const sequelize = new Sequelize('learn-node-js-max', 'root', '12345678', {
   dialect: 'mysql',
