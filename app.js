3f05a0a53029e3f0d4ac08df2330529f1c1fe0cb
BayC <rachada_32@hotmail.com>
2023-09-09 23:50:30 -0400

    Add relations between Cart, User, Product, and CartItem

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const sequelize = require('./utils/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -43,11 +45,15 @@ app.use('/', errorController.get404);
 // Associations/Relations
 Product.belongsTo(User, { constrains: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 const port = 3000;
 sequelize
-  // .sync({ force: true }) // force override tables with updated script, won't use in production
-  .sync() // this is run when app started
+  .sync({ force: true }) // force override tables with updated script, won't use in production
+  // .sync() // this is run when app started
   .then((result) => {
     // check for a non-existing user to create a dummy user since we have no UI to create a user yet
     return User.findByPk(1);
