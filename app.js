ce74a1aaf3c91f37be77166451819f3d98a1965e
BayC <rachada_32@hotmail.com>
2023-09-12 00:06:01 -0400

    Add relationships between Order, User, Product, and OrderItem

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -45,10 +47,17 @@ app.use('/', errorController.get404);
 // Associations/Relations
 Product.belongsTo(User, { constrains: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+
+// Cart
 User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+
+// Order
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 const port = 3000;
 sequelize
