91b095517778e2832cca8734eb81f73d470df99e
BayC <rachada_32@hotmail.com>
2023-09-16 23:52:32 -0400

    Create new order with products from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -99,6 +99,31 @@ exports.postCart = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          const cartProducts = products.map((product) => {
+            product.orderItem = { quantity: product.cartItem.quantity };
+            return product;
+          });
+          return order.addProducts(cartProducts);
+        })
+        .catch((err) => console.log(err));
+    })
+    .then((result) => {
+      res.redirect('orders');
+    })
+
+    .catch((err) => console.log(err));
+};
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
--- routes/shop.js
+++ routes/shop.js
@@ -11,6 +11,7 @@ router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 router.get('/checkout', shopController.getCheckout);
 router.get('/orders', shopController.getOrders);
+router.post('/create-order', shopController.postOrder);
 router.post('/cart-delete-item', shopController.postCartDeleteItem);
 
 module.exports = router;
