be6dd6339140167c73ccb62c2d98ea775fe2bbb3
BayLadyCoder <rachada_32@hotmail.com>
2023-12-15 19:34:59 -0500

    Add multiple products to cart

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ class User {
   constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
-    this.cart = cart;
+    this.cart = cart?.items ? cart : { items: [] };
     this._id = id;
   }
 
@@ -15,11 +15,26 @@ class User {
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex((cProduct) => {
-    //   return cProduct.id === product.id;
-    // });
+    const cartProductIndex = this.cart.items.findIndex((cProduct) => {
+      return cProduct.productId.toString() === product._id.toString();
+    });
+
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      // update existing product in cart
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: product._id,
+        quantity: newQuantity,
+      });
+    }
+
     const updatedCart = {
-      items: [{ productId: new ObjectId(product.id), quantity: 1 }],
+      items: updatedCartItems,
     };
     const db = getDb();
     return db.collection('users').updateOne(
