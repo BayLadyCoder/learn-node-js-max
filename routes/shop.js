e0cd6b368c40446939874277f23eccdded5ce69d
BayLadyCoder <rachada_32@hotmail.com>
2023-12-17 00:08:05 -0500

    Show each product details

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,7 @@ const shopController = require('../controllers/shop');
 
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
-// router.get('/products/:productId([0-9a-fA-F]{24})', shopController.getProduct);
+router.get('/products/:productId([0-9a-fA-F]{24})', shopController.getProduct);
 // router.get('/cart', shopController.getCart);
 // router.post('/cart', shopController.postCart);
 // // router.get('/checkout', shopController.getCheckout);
