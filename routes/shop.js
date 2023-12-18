677c690082489d8732c396ce975ce030643c6e11
BayLadyCoder <rachada_32@hotmail.com>
2023-12-18 17:39:51 -0500

    Add deleteItemFormCart() method to userSchema and fix bug in cart.ejs

--- routes/shop.js
+++ routes/shop.js
@@ -9,9 +9,9 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId([0-9a-fA-F]{24})', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
-// // router.get('/checkout', shopController.getCheckout);
+// router.get('/checkout', shopController.getCheckout);
 // router.get('/orders', shopController.getOrders);
 // router.post('/create-order', shopController.postOrder);
-// router.post('/cart-delete-item', shopController.postCartDeleteItem);
+router.post('/cart-delete-item', shopController.postCartDeleteItem);
 
 module.exports = router;
