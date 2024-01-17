81c94386de1a6fa1352126f19e6f943ea4976cff
BayLadyCoder <rachada_32@hotmail.com>
2024-01-16 23:23:02 -0500

    Use fetch with DELETE method to delete a product and client update UI after deleting is done

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,20 @@
 const deleteProduct = (btn) => {
   const productId = btn.parentNode.querySelector('[name=id]').value;
   const csrfToken = btn.parentNode.querySelector('[name=_csrf]').value;
-  console.log('clicked', { productId, csrfToken });
+  const productElement = btn.parentNode.parentNode;
+
+  fetch(`/admin/products/${productId}`, {
+    method: 'DELETE',
+    headers: {
+      'csrf-token': csrfToken,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log({ data });
+      productElement.remove();
+    })
+    .catch((err) => console.log({ err }));
 };
